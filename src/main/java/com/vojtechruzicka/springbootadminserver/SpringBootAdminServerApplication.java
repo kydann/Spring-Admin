66f515ded8bb07baf1194473bc16d46fd4cc0d08
kydann <kydann.2907@gmail.com>
2018-12-27 13:19:09 -0600

    Administrador Spring-Boot

---
+++ src/main/java/com/vojtechruzicka/springbootadminserver/SpringBootAdminServerApplication.java
@@ -0,0 +1,14 @@
+package com.vojtechruzicka.springbootadminserver;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAdminServer
+public class SpringBootAdminServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAdminServerApplication.class, args);
+    }
+}
